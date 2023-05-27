0e5411c7c06d4c7d695325f567daa28fac257b5d
Camilo Avila <camilo.avil@gmail.com>
2023-05-27 12:50:23 -0500

    definen parametros

--- app/removeBack/removebg.py
+++ app/removeBack/removebg.py
@@ -4,18 +4,26 @@
 #the code was provided by remove.bg webpage
 
 # Requires "requests" to be installed (see python-requests.org)
-import requests
+import requests, os
+from app.removeBack import removebg_APIkey
 
+urlRemoveBg = 'https://api.remove.bg/v1.0/removebg'
 
-def removeBG_one_pic():
-    response = requests.post(
-        'https://api.remove.bg/v1.0/removebg',
-        files={'image_file': open('/path/to/file.jpg', 'rb')},
-        data={'size': 'auto'},
-        headers={'X-Api-Key': 'INSERT_YOUR_API_KEY_HERE'},
-    )
-    if response.status_code == requests.codes.ok:
-        with open('no-bg.png', 'wb') as out:
-            out.write(response.content)
+def removeBG_one_pic(url_jpg: str):
+    print(f"let's remove background of {url_jpg}")
+    if isinstance(url_jpg, str) and os.path.isfile(url_jpg) and url_jpg.lower().endswith(".jpg"):
+        print('The file is correct')
+        print(f'My APIkey {removebg_APIkey}')
     else:
-        print("Error:", response.status_code, response.text)
+        print('Sorry parameter incorrect')
+        # response = requests.post(
+        #     urlRemoveBg,
+        #     files={'image_file': open('/path/to/file.jpg', 'rb')},
+        #     data={'size': 'auto'},
+        #     headers={'X-Api-Key': 'INSERT_YOUR_API_KEY_HERE'},
+        # )
+        # if response.status_code == requests.codes.ok:
+        #     with open('no-bg.png', 'wb') as out:
+        #         out.write(response.content)
+        # else:
+        #     print("Error:", response.status_code, response.text)
--- main.py
+++ main.py
@@ -1,40 +1,7 @@
-from app.detection.box import Box
-from app.detection.facedetetion import scaning_dir, filterbysize
-import cv2
-import sys, os
-
-def funtion(file):
-    print(f'File location: {file}')
-    image = cv2.imread(file)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.2,
-        minNeighbors=3,
-        minSize=(30, 30)
-    )
-    faces = filterbysize(faces,(image.shape[0] * image.shape[1]),verbose=True)  #Filter by Total area relation
-    new_faces = []
-    # check intersections
-    for i in range(len(faces)):
-        for j in range(i + 1,len(faces)):
-            if faces[i].intercepts(faces[j]) is False:
-                new_faces.append(faces[i])
-            # print(f'{i} con {j}')
-            # print(f'Intercepcion ',faces[i].intercepts(faces[j]))
-    print(faces)
-    for face in faces:
-        print (f'S point {face.get_sPoint()} E point {face.get_ePoint()}')
-        cv2.rectangle(image, face.get_sPoint(), face.get_ePoint(), (0, 255, 0), 5)
-        face.get_amplify(125)
-        print (f'S point {face.get_sPoint()} E point {face.get_ePoint()}')
-        cv2.rectangle(image, face.get_sPoint(), face.get_ePoint(), (255, 0, 0), 5)
-        print('\n')
-    status = cv2.imwrite('faces_detected.jpg', image)
-    print ("Image faces_detected.jpg written to filesystem: ",status)
-
+from app.detection.facedetetion import scaning_dir
+from app.removeBack.removebg import removeBG_one_pic
 
 if __name__ == '__main__':
     # numberPath = sys.argv[1]
-    scaning_dir(f'img/input/', testing=False)
+    # scaning_dir(f'img/input/', testing=False)
+    removeBG_one_pic(32)
